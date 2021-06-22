cc1ed17eff5206b810e371aeef4134cab6750a2b
tan <tanmaster95@hotmail.com>
2021-06-22 21:08:56 +0200

    add project files

---
+++ main.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+#
+# Copyright 2018 Joachim Lusiardi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import argparse
+import logging
+import os.path
+from ctypes import cast, POINTER
+
+import comtypes
+from comtypes import CLSCTX_ALL
+from homekit import AccessoryServer
+from homekit.model import Accessory
+from homekit.model.characteristics import BrightnessCharacteristic
+from homekit.model.services import LightBulbService
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, EDataFlow, ERole, CLSID_MMDeviceEnumerator, \
+    IMMDeviceEnumerator
+
+# setup logger
+logger = logging.getLogger('accessory')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter('%(asctime)s %(filename)s:%(lineno)04d %(levelname)s %(message)s'))
+logger.addHandler(ch)
+logger.info('starting')
+
+
+class MyAudioUtilities(AudioUtilities):
+    @staticmethod
+    def GetDeviceEnumerator():
+        return comtypes.CoCreateInstance(
+            CLSID_MMDeviceEnumerator,
+            IMMDeviceEnumerator,
+            comtypes.CLSCTX_INPROC_SERVER)
+
+
+deviceEnumerator = MyAudioUtilities.GetDeviceEnumerator()
+
+
+def vol_switched(new_value):
+    global oldValue
+    logger.info('=======>  light switched: {x}'.format(x=new_value))
+
+
+def vol_changed(new_value):
+    logger.info('=======>  light changed: {x}'.format(x=new_value))
+    # Get default audio device using PyCAW
+    devices = deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender.value, ERole.eMultimedia.value)
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    # Get current volume
+    volume.SetMasterVolumeLevelScalar(new_value / 100, None)
+
+
+def setup_args_parser():
+    parser = argparse.ArgumentParser(description='HomeKit demo server')
+    parser.add_argument('-f', action='store', required=False, dest='file',
+                        default='./demoserver.json',
+                        help='File with the config data (defaults to ./demoserver.json)')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = setup_args_parser()
+
+    config_file = os.path.expanduser(args.file)
+
+    # create a server and an accessory an run it unless ctrl+c was hit
+    try:
+        httpd = AccessoryServer(config_file, logger)
+
+        accessory = Accessory('PC Volume', 'Tan', 'Das Gute', '0001', '0.1')
+        lightBulbService = LightBulbService()
+
+        lightBulbService.set_on_set_callback(vol_switched)
+        bchar = BrightnessCharacteristic(1)
+        bchar.set_set_value_callback(vol_changed)
+
+        lightBulbService.append_characteristic(bchar)
+
+        accessory.services.append(lightBulbService)
+        httpd.add_accessory(accessory)
+
+        httpd.publish_device()
+
+        logger.info('published device and start serving')
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    # unpublish the device and shut down
+    logger.info('unpublish device')
+    httpd.unpublish_device()
+    httpd.shutdown()
